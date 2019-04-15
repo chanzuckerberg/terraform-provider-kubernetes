c1c663fd1847e625cb99d9b638cf4a3cc692d6f0
Ryan King <rking@chanzuckerberg.com>
2019-04-15 15:09:52 -0700

    fix ForceNew login on deployment
    Based on the suggestion [at](https://github.com/terraform-providers/terraform-provider-kubernetes/issues/201#issuecomment-478613255) this will allow deployments to be updated in place when an environment variable changes.

--- kubernetes/schema_container.go
+++ kubernetes/schema_container.go
@@ -222,7 +222,7 @@ func containerFields(isUpdatable, isInitContainer bool) map[string]*schema.Schem
 					},
 					"value": {
 						Type:        schema.TypeString,
-						ForceNew:    true,
+						ForceNew:    !isUpdatable,
 						Optional:    true,
 						Description: `Variable references $(VAR_NAME) are expanded using the previous defined environment variables in the container and any service environment variables. If a variable cannot be resolved, the reference in the input string will be unchanged. The $(VAR_NAME) syntax can be escaped with a double $$, ie: $$(VAR_NAME). Escaped references will never be expanded, regardless of whether the variable exists or not. Defaults to "".`,
 					},
